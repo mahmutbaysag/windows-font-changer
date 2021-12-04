ee4434cd781636cfba9b21f10481c9c4aedb4a0a
Mahmut Baysağ <mbaysag1080@gmail.com>
2021-12-04 16:26:25 +0300

    Update

---
+++ Windows Font Changer/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Windows_Font_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 100;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(12, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(372, 33);
+            this.comboBox1.Sorted = true;
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Değiştir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Trebuchet MS", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(229, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sıfırla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(344, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "X";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS PGothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(60, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Windows Font Changer";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(17, 163);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(367, 101);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(400, 280);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Windows Font Changer/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Windows_Font_Changer
+{
+    public partial class Form1 : Form
+    {
+        private bool _dragging = false;
+        private Point _offset;
+        private Point _start_point = new Point(0, 0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        RegistryKey register;
+
+        public static bool checkMachineType()
+        {
+            RegistryKey winLogonKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes", true);
+            return (winLogonKey.GetValueNames().Contains("Segoe UI"));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CultureInfo ci = CultureInfo.InstalledUICulture;
+            
+            switch(CultureInfo.CurrentCulture.Name)
+            {
+                case "tr-TR":
+                    Localization.Culture = new CultureInfo("tr-TR");
+                    break;
+                default:
+                    Localization.Culture = new CultureInfo("en-US");
+                    break;
+            }
+
+            button1.Text = Localization.degistir;
+            button2.Text = Localization.sifirla;
+
+            InstalledFontCollection installedFontCollection = new InstalledFontCollection();
+            FontFamily[] fontlar = installedFontCollection.Families;
+
+            foreach(FontFamily i in fontlar)
+            {
+                comboBox1.Items.Add(i.Name);
+            }
+
+            register = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes", true);
+            if (!checkMachineType())
+            {
+                comboBox1.Text = "Segoe UI";
+            }
+            else
+            {
+                comboBox1.Text = register.GetValue("Segoe UI").ToString();
+            }
+
+        }
+
+        void fontDegistir(string fontName)
+        { 
+            
+            register = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts",true);
+            register.SetValue("Segoe UI (TrueType)","");
+            register.SetValue("Segoe UI Bold (TrueType)", "");
+            register.SetValue("Segoe UI Bold Italic(TrueType)", "");
+            register.SetValue("Segoe UI Italic (TrueType)", "");
+            register.SetValue("Segoe UI Semibold (TrueType)", "");
+            register.SetValue("Segoe UI Symbol (TrueType)", "");
+            
+            register = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes",true);
+            register.SetValue("Segoe UI", fontName);
+        }  
+        
+        void fontSifirla()
+        {
+            
+            register = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts",true);
+            register.SetValue("Segoe UI (TrueType)", "segoeui.ttf");
+            register.SetValue("Segoe UI Black (TrueType)", "seguibl.ttf");
+            register.SetValue("Segoe UI Black Italic (TrueType)", "seguibli.ttf");
+            register.SetValue("Segoe UI Bold (TrueType)", "segoeuib.ttf");
+            register.SetValue("Segoe UI Bold Italic (TrueType)", "segoeuiz.ttf");
+            register.SetValue("Segoe UI Emoji (TrueType)", "seguiemj.ttf");
+            register.SetValue("Segoe UI Historic (TrueType)", "seguihis.ttf");
+            register.SetValue("Segoe UI Italic (TrueType)", "segoeuii.ttf");
+            register.SetValue("Segoe UI Light (TrueType)", "segoeuil.ttf");
+            register.SetValue("Segoe UI Light Italic (TrueType)", "seguili.ttf");
+            register.SetValue("Segoe UI Semibold (TrueType)", "seguisb.ttf");
+            register.SetValue("Segoe UI Semibold Italic (TrueType)", "seguisbi.ttf");
+            register.SetValue("Segoe UI Semilight (TrueType)", "segoeuisl.ttf");
+            register.SetValue("Segoe UI Semilight Italic (TrueType)", "seguisli.ttf");
+            register.SetValue("Segoe UI Symbol (TrueType)", "seguisym.ttf");
+            register.SetValue("Segoe MDL2 Assets (TrueType)", "segmdl2.ttf");
+            register.SetValue("Segoe Print (TrueType)", "segoepr.ttf");
+            register.SetValue("Segoe Print Bold (TrueType)", "segoeprb.ttf");
+            register.SetValue("Segoe Script (TrueType)", "segoesc.ttf");
+            register.SetValue("Segoe Script Bold (TrueType)", "segoescb.ttf");
+
+            register = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontSubstitutes",true);
+            if(register.GetValue("Segoe UI")!=null)
+            {
+                register.DeleteValue("Segoe UI");
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fontDegistir(comboBox1.SelectedItem.ToString());
+            MessageBox.Show(Localization.yazitipiDegistirMesaj,Localization.mesajBaslik);
+            restartCheck();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fontSifirla();
+            MessageBox.Show(Localization.yazitipiSifirlaMesaj, Localization.mesajBaslik);
+            restartCheck();
+        }
+
+        void restartCheck()
+        {
+            if(MessageBox.Show(Localization.yenidenBaslatMesaj, Localization.yenidenBaslatBaslik, MessageBoxButtons.YesNo, MessageBoxIcon.Information)==DialogResult.Yes)
+            {
+                Process.Start("shutdown.exe","-r -t 0");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Font = new Font(comboBox1.Text,15);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1_TextChanged(sender,e);
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            comboBox1_TextChanged(sender,e);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;  // _dragging is your variable flag
+            _start_point = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
+        }
+    }
+}
+
+
